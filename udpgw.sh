677f5052c653d16b8487dc9516fec2d368bdd763
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 01:53:42 +0330

    Update udpgw.sh

--- udpgw.sh
+++ udpgw.sh
@@ -1,10 +1,10 @@
-wget -O /usr/bin/badvpn-udpgw "https://github.com/AmirShams-ir/UDPGW/raw/main/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-wget -O /usr/bin/badvpn-udpgw "https://github.com/AmirShams-ir/UDPGW/raw/main/badvpn-udpgw64"
+wget -O /usr/bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw64"
 fi
-wget -O /bin/badvpn-udpgw "https://github.com/AmirShams-ir/UDPGW/raw/main/badvpn-udpgw"
+wget -O /bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-wget -O /bin/badvpn-udpgw "https://github.com/AmirShams-ir/UDPGW/raw/main/badvpn-udpgw64"
+wget -O /bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw64"
 fi
 touch /etc/rc.local
 sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
