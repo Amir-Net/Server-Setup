d5b59047c88af81707d38d803ea9dc29868bdc92
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-17 22:59:22 +0330

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,4 +1,11 @@
-su root
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script will be run with sudo privileges."
+  else
+    echo "This script must be run with sudo privileges."
+    exit 1
+  fi
+fi
 fallocate -l 1G /swapfile
 dd if=/dev/zero of=/swapfile bs=1024 count=1048576
 chmod 600 /swapfile
