524a79f9472b04531e67cd84d5037255d098eaf5
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 22:58:33 +0330

    Update tls.sh

--- tls.sh
+++ tls.sh
@@ -1,6 +1,6 @@
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
 cat key.pem cert.pem >> full.pem
-mv -i key.pem ~/etc/ssl/certs
-mv -i cert.pem ~/etc/ssl/certs
-mv -i full.pem ~/etc/ssl/certs
+mv -i key.pem /etc/ssl/certs
+mv -i cert.pem /etc/ssl/certs
+mv -i full.pem /etc/ssl/certs
