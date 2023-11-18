39e4ea7efd39d9abb8fa60fb5d1036d446dc8af5
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 23:07:12 +0330

    Update udpgw.sh

--- udpgw.sh
+++ udpgw.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
+# -----------------------------------------------------------------------------
+# Description: This script automates the setup and configuration of various
+#              utilities and services on a Linux server for a secure and
+#              optimized environment.
+#
+# Author: BabyBoss
+# GitHub: https://github.com/Amir-Net/
+#
+# Disclaimer: This script is provided for educational and informational
+#             purposes only. Use it responsibly and in compliance with all
+#             applicable laws and regulations.
+#
+# Note: Make sure to review and understand each section of the script before
+#       running it on your system. Some configurations may require manual
+#       adjustments based on your specific needs and server setup.
+# -----------------------------------------------------------------------------
+# Check for sudo privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script will be run with sudo privileges."
+  else
+    echo "This script must be run with sudo privileges."
+    exit 1
+  fi
+fi
 clear
+
+#install UDPGW
 udpport=7300
 echo -e "\nPlease input UDPGW Port ."
 printf "Default Port is \e[33m${udpport}\e[0m, let it blank to use this Port: "
