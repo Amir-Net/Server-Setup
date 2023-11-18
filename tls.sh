9311820898255e4fe5c24d7d0ab8c894f4f05d18
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 23:04:07 +0330

    Update tls.sh

--- tls.sh
+++ tls.sh
@@ -1,6 +1,36 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Description: This script automates the setup and configuration of various
+#              utilities and services on a Linux server for a secure and
+#              optimized environment.
+#
+# Author: BabyBoss
+# GitHub: https://github.com/Amir-Net/
+#
+# Disclaimer: This script is provided for educational and informational
+#             purposes only. Use it responsibly and in compliance with all
+#             applicable laws and regulations.
+#
+# Note: Make sure to review and understand each section of the script before
+#       running it on your system. Some configurations may require manual
+#       adjustments based on your specific needs and server setup.
+# -----------------------------------------------------------------------------
+# Check for sudo privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script will be run with sudo privileges."
+  else
+    echo "This script must be run with sudo privileges."
+    exit 1
+  fi
+fi
+clear
+
+# install Self-certificate
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
-cat key.pem cert.pem >> full.pem
-mv -i key.pem /etc/ssl/certs
-mv -i cert.pem /etc/ssl/certs
-mv -i full.pem /etc/ssl/certs
+sudo cat key.pem cert.pem >> full.pem
+sudo cat full.pem
+sudo mv -i key.pem /etc/ssl/certs
+sudo mv -i cert.pem /etc/ssl/certs
+sudo mv -i full.pem /etc/ssl/certs
