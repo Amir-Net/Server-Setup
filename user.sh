1d049598dbfa8b0d41aef7aa5a70ccdf52e10146
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 21:08:35 +0330

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,4 +1,39 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Description: This script automates the setup and configuration of various
+#              utilities and services on a Linux server for a secure and
+#              optimized environment.
+#
+# Author: BabyBoss
+# GitHub: https://github.com/Amir-Net/
+#
+# Disclaimer: This script is provided for educational and informational
+#             purposes only. Use it responsibly and in compliance with all
+#             applicable laws and regulations.
+#
+# Note: Make sure to review and understand each section of the script before
+#       running it on your system. Some configurations may require manual
+#       adjustments based on your specific needs and server setup.
+# -----------------------------------------------------------------------------
+# Check for sudo privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script will be run with sudo privileges."
+  else
+    echo "This script must be run with sudo privileges."
+    exit 1
+  fi
+fi
 clear
-read -p "Enter Your username: " name
-adduser $name --shell /usr/sbin/nologin
-unset name
+
+# read on-login users
+  read -p  "Enter Your usernames (comma-separated, e.g.):" names
+  
+  # Add names to the user
+  if [ -n "$names" ]; then
+    IFS=',' read -ra names_array <<< "$names"
+    for port in "${names_array[@]}"; do
+      adduser $names --shell /usr/sbin/nologin
+    done
+  fi
+ unset names
