5998e0742340545bb218e937db269e9aaddc73c0
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 03:43:01 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,3 +1,12 @@
+# Check for sudo privileges
+if [[ $EUID -ne 0 ]]; then
+  if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "This script will be run with sudo privileges."
+  else
+    echo "This script must be run with sudo privileges."
+    exit 1
+  fi
+fi
 # Add BBR settings to sysctl.conf
 echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf
