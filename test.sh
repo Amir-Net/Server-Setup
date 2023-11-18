f565c18ede60113407d4c6ce50cd13b35a7fb0ab
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 03:31:30 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
-if [[ $EUID -ne 0 ]]; then
-  if [[ $(sudo -n true 2>/dev/null) ]]; then
-    echo "This Script Will be Run with sudo Privileges."
-  else
-    echo "This Script Must be Run with sudo Privileges."
-    echo "Please Enter Root Password."
-    su root
-    11g
-  fi
-fi
+if [[ $EUID -ne 0 ]]; 
+then
 apt update
 apt upgrade
 apt autoremove
 apt autoclean
 apt clean
+  else
+   echo "This Script Must be Run with sudo Privileges."
+   echo "Please Enter Root Password."
+   su root
+   apt update
+   apt upgrade
+   apt autoremove
+   apt autoclean
+   apt clean
+  fi
