11f6f6194c15f4bbd07b030c49c599dfe4967d8f
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 03:51:05 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# Description: This script automates the setup and configuration of various
+#              utilities and services on a Linux server for a secure and
+#              optimized environment.
+#
+# Author: BabyBoss
+# GitHub: https://github.com/Amir-Net/
+#
+# Disclaimer: This script is provided for educational and informational
+#             purposes only. Use it responsibly and in compliance with all
+#             applicable laws and regulations.
+#
+# Note: Make sure to review and understand each section of the script before
+#       running it on your system. Some configurations may require manual
+#       adjustments based on your specific needs and server setup.
+# -----------------------------------------------------------------------------
 # Check for sudo privileges
 if [[ $EUID -ne 0 ]]; then
   if [[ $(sudo -n true 2>/dev/null) ]]; then
