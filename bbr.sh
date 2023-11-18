940c196e3c8d81dcc268a6852dbeb8e892294607
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-18 03:47:26 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -12,4 +12,4 @@ echo "net.core.default_qdisc = fq" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr" | sudo tee -a /etc/sysctl.conf
 # Apply the new settings
 sudo sysctl -p
-sysctl net.ipv4.tcp_congestion_control
+sudo sysctl net.ipv4.tcp_congestion_control
