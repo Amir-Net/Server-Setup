82691cec88220f37b619a17aa7cb02cbaaa3893d
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2023-11-19 22:25:05 +0330

    Update and rename udpgw2.sh to corn.sh

--- corn.sh
+++ corn.sh
@@ -26,27 +26,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 clear
 
-# setup udpgw
-wget -O /usr/bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw"
-if [ "$OS" == "x86_64" ]; then
-wget -O /usr/bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw64"
-fi
-wget -O /bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw"
-if [ "$OS" == "x86_64" ]; then
-wget -O /bin/badvpn-udpgw "https://github.com/Amir-Net/Server-Setup/raw/main/badvpn-udpgw64"
-fi
-
-# port BadVPN 7300
-sed -i '$ i\screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
-' /etc/rc.local
-
-# permission
-chmod +x /usr/bin/badvpn-udpgw
-chmod +x /bin/badvpn-udpgw
-chmod +x /etc/rc.local
-
-# active badvpn 7300
-screen -dmS udpvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
-systemctl enable rc-local
-systemctl start rc-local.service
-systemctl status rc-local.service
+# setup corn
+sudo systemctl enable cron
+crontab -e
+00 22 * * * /usr/bin/apt-get update && /usr/bin/apt-get upgrade -y && /usr/bin/apt-get autoremove -y && /usr/bin/apt-get autoclean -y && /usr/bin/apt-get clean -y
+30 22 * * * /sbin/shutdown -r
