894bffd6746ebf0b860bc579db07542482850a66
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2024-02-01 23:26:53 +0330

    Update website.sh

--- website.sh
+++ website.sh
@@ -26,41 +26,55 @@ if [[ $EUID -ne 0 ]]; then
 fi
 clear
 
-# Setup nginx
-sudo apt update -y
-sudo apt install -y nginx
-sudo ufw allow 'Nginx HTTP'
-sudo ufw allow 'Nginx HTTPS'
-sudo ufw allow 'Nginx Full'
-sudo systemctl start nginx
-sudo systemctl enable nginx
-read -p  "Enter your domain adress:" domain
-sudo mkdir -p /var/www/$domain/html
-sudo chown -R $USER:$USER /var/www/$domain/html
-sudo chmod -R 755 /var/www/$domain
-sudo touch /var/www/$domain/html/index.html
-sudo cat << EOF >> /etc/nginx/sites-available/$domain
-server {
-        listen 80;
-        listen [::]:80;
-        root /var/www/$domain/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name $domain www.$domain;
-        location / {
-        try_files $uri $uri/ =404;
-        }
-       }
-EOF
-sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled
-sudo nginx -t
-sudo systemctl restart nginx
-systemctl status nginx
+  # Setup nginx
+  sudo apt update -y
+  sudo apt install -y nginx
+  sudo apt install -y ufw
+  sudo ufw allow 'Nginx HTTP'
+  sudo ufw allow 'Nginx HTTPS'
+  sudo ufw allow 'Nginx Full'
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
+  read -p  "Enter your domain adress:" domain
+  sudo mkdir -p /var/www/$domain/html
+  sudo chown -R $USER:$USER /var/www/$domain/html
+  sudo chmod -R 755 /var/www/$domain
+  sudo touch /var/www/$domain/html/index.html
+  sudo cat << EOF >> /etc/nginx/sites-available/$domain
+  server {
+          listen 80;
+          listen [::]:80;
+          root /var/www/$domain/html;
+          index index.html index.htm index.nginx-debian.html;
+          server_name $domain www.$domain;
+          location / {
+          try_files $uri $uri/ =404;
+          }
+         }EOF
+  sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled
+  sudo nginx -t
+  sudo systemctl restart nginx
+  systemctl status nginx
 
-# Setup TLS for nginx
-sudo apt install -y certbot python3-certbot-nginx
-sudo nginx -t
-sudo systemctl reload nginx
-sudo certbot --nginx -d $domain -d www.$domain
-sudo systemctl status certbot.timer
-sudo certbot renew --dry-run
-unset domain
+  # install certificate
+  sudo apt install -y certbot
+  echo "SSL/TLS Certificates by Letsencrypt"
+  read -p "Enter your email:" email
+  read -p "Enter your domain (e.g., sub.domain.com):" sni
+  if [ -n "$email" ] && [ -n "$domain" ]; then
+  sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email "$email" -d "$domain"
+  echo "SSL/TLS certificates obtained successfully for $domain in /etc/letsencrypt/live."
+  unset email
+  unset sni
+  else
+  echo "Both email and domain are required to obtain SSL certificates."
+  fi
+  
+  # Setup TLS for nginx
+  sudo apt install -y certbot python3-certbot-nginx
+  sudo nginx -t
+  sudo systemctl reload nginx
+  sudo certbot --nginx -d $domain -d www.$domain
+  sudo systemctl status certbot.timer
+  sudo certbot renew --dry-run
+  unset domain
