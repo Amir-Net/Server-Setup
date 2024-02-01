5fe31356822e83cf1c9ea6a02a53f5cf9a2996ca
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2024-02-01 03:51:32 +0330

    Update web.sh

--- web.sh
+++ web.sh
@@ -35,7 +35,6 @@ sudo ufw allow 'Nginx Full'
 sudo curl -4 icanhazip.com
 sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo rm -r /var/www/html
 read -p  "Enter your domain adress:" domain
 sudo mkdir -p /var/www/$domain/html
 sudo chown -R $USER:$USER /var/www/$domain/html
