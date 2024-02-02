476c71e9998fc18e5b6da8c3b60636088f89f314
BabyBoss <151217448+Amir-Net@users.noreply.github.com>
2024-02-02 04:35:04 +0330

    Update web.sh

--- web.sh
+++ web.sh
@@ -33,35 +33,29 @@ clear
  read -p "Enter your email:" email
  read -p "Enter your domain adress:" domain
  if [ -n "$email" ] && [ -n "$domain" ]; then
- sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email "$email" -d "$domian"
+ sudo certbot certonly --standalone --preferred-challenges http --email "$email" -d "$domain"
  echo "SSL/TLS certificates obtained successfully for $domain in /etc/letsencrypt/live."
  else
  echo "Both email and domain are required to obtain SSL certificates."
  fi
+ 
+ # Setup nginx
+ sudo apt install -y nginx ufw
+ sudo ufw allow 'Nginx Full'
+ sudo systemctl start nginx
+ sudo systemctl enable nginx
 
-# Setup nginx
-sudo apt update -y
-sudo apt install -y nginx ufw
-sudo ufw allow 'Nginx Full'
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-# Create domain directories
-sudo mkdir -p /var/www/$domain/html
-sudo chown -R $USER:$USER /var/www/$domain/html
-sudo chmod -R 755 /var/www/$domain
-
-# Create index.html
-cat << EOF > /var/www/$domain/html/index.html
-<!DOCTYPE html>
-<html lang="en">
-<head>
+ # Create index.html
+ cat << EOF > /var/www/html/index.html
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Coming Soon | Your Website Name</title>
+  <title>Coming Soon | $domain</title>
   <link rel="stylesheet" href="style.css">
-</head>
-<body>
+ </head>
+ <body>
   <div class="container">
     <header>
       <h1>Something amazing is coming soon!</h1>
@@ -90,44 +84,6 @@ cat << EOF > /var/www/$domain/html/index.html
       </div>
     </footer>
   </div>
-</body>
-</html>
-EOF
-
-# Create Nginx configuration
-cat << EOF > /etc/nginx/sites-available/$domain
-server {
-    listen 80;
-    listen [::]:80;
-    server_name $domain www.$domain;
-
-    # Redirect to HTTPS
-    return 301 https://$server_name$request_uri;
-}
-
-server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name $domain www.$domain;
-
-    # SSL/TLS configuration
-    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
-
-    root /var/www/$domain/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-EOF
-
-sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled
-sudo nginx -t
-sudo systemctl restart nginx
-
-# Setup TLS for nginx
-sudo certbot --nginx -d $domain -d www.$domain
-sudo systemctl status certbot.timer
-sudo certbot renew --dry-run
+ </body>
+ </html>
+ EOF
